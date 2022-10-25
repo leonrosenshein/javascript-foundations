5e51a9de7115d5fdf9ec982a626f609e6c4a6330
Leon Rosenshein <leonrosn@amazon.com>
2022-10-25 14:34:37 -0600

    Implement dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
 
+class Dragon {
+    constructor(dragonName, riderName) {
+        this.name = dragonName
+        this.rider = riderName
+        this.hungry = true
+        this.eatCount = 0
+    }
+
+    eat() {
+        this.eatCount++
+        if (this.eatCount >= 3) {
+            this.hungry = false
+        }
+    }
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+}
+
+module.exports = Dragon
